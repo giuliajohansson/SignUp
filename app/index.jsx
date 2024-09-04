eb47a7554ac0f4ee4330f4d8686ab8e477bd12e4
giuliajohansson <giulia_rodrigues@estudante.sesisenai.org.br>
2024-09-04 08:41:54 -0300

    commit

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View,Text } from 'react-native'
+
+const SignUp = function () {
+    return (
+        <View>
+            <Text>oi</Text>
+        </View>
+    )
+}
+
+export default SignUp
